1140f5e29c5b171160f6a11bffde4534a71abc5b
Wasim Shaikh <shaikhwasim78@gmail.com>
2020-09-30 23:35:14 +0530

    -subject changed to project

---
+++ src/main/java/com/albraik/task/project/service/ProjectTypeService.java
@@ -0,0 +1,11 @@
+package com.albraik.task.project.service;
+
+import java.util.List;
+
+import com.albraik.task.project.model.ProjectTypeEntity;
+
+public interface ProjectTypeService {
+	
+	List<ProjectTypeEntity> getAllProjectType();
+	
+}
